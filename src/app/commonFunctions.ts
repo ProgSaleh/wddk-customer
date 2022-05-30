fc11039c94ed0a027fbc55b035a669d1e1b40684
ProgSaleh <progsaleh@hotmail.com>
2022-05-31 01:36:58 +0300

    setup and project's common functions

---
+++ src/app/commonFunctions.ts
@@ -0,0 +1,13 @@
+/**
+ * @param input value to be verified.. might be $event(to be handled later)
+ *
+ * @return true if ALL input chars are of number, else otherwise.
+ */
+export function verifyOnlyNums(input: string): boolean {
+  if (input) {
+    if (input.length) {
+    }
+  }
+
+  return false;
+}
